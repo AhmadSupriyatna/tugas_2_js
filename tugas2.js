0d9c4a7bf16fe7d9f731b457dc06b46498a5b9dc
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-18 21:55:56 +0700

    Tugas 2 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+var stok = 6;
+var namaBarang = 'Lamborgini';
+
+if (stok>0)
+    status = true;
+else
+    status = false;
+
+var type = null;
+var warna;
+
+
+console.log(stok);
+console.log(namaBarang);
+console.log(status);
+console.log(type);
+console.log(warna);
